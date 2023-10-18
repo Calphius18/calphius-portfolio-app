0b7004450e59346107f006c7725ff6dde682b1de
Femi <fagbemibabs18@gmail.com>
2023-10-18 17:44:06 +0100

    Directories for the Microservices

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,6 +19,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + '/views/timestamp.html');
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
